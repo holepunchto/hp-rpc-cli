83fc99058d4d1821a88cb79f58fb375500aa82df
Paolo Ardoino <paolo@tether.to>
2023-07-13 16:35:11 +0200

    support for identity and timeout

--- client.js
+++ client.js
@@ -1,4 +1,7 @@
 const RPC = require('@hyperswarm/rpc')
+const libUtils = require('@hyper-cmd/lib-utils')
+const libKeys = require('@hyper-cmd/lib-keys')
+
 const argv = require('minimist')(process.argv.slice(2))
 
 const helpMsg = 'Usage:\nhp-rpc-cli ?-i identity.json ?-s peer_key -m method -d data'
@@ -23,14 +26,33 @@ if (!argv.d) {
   process.exit(-1)
 }
 
+let keyPair = null
+
+if (argv.i) {
+  keyPair = libUtils.resolveIdentity([], argv.i)
+
+  if (!keyPair) {
+    console.error('Error: identity file invalid')
+    process.exit(-1)
+  }
+
+  keyPair = libKeys.parseKeyPair(keyPair)
+}
+
 const rpc = new RPC({
-  
+  keyPair
 })
+
+if (!argv.t) {
+  argv.t = 5000
+}
 
 const client = rpc.connect(Buffer.from(argv.s, 'hex'));
 
 (async () => {
-  const res = await client.request(argv.m, Buffer.from(argv.d))
+  const res = await client.request(argv.m, Buffer.from(argv.d), {
+    timeout: argv.t
+  })
 
   console.log(res.toString())
 })()

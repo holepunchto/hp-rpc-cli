f9cc3792a5886da59a552b5cb8c10bebe25dd1a8
vigan-abd <vigan.abd@gmail.com>
2023-09-29 11:11:02 +0200

    moved args outside

--- client.js
+++ client.js
@@ -8,65 +8,72 @@ const RPC = require('@hyperswarm/rpc')
 const libUtils = require('@hyper-cmd/lib-utils')
 const libKeys = require('@hyper-cmd/lib-keys')
 
+const argv = require('minimist')(process.argv.slice(2))
+
+const helpMsg = 'Usage:\nhp-rpc-cli ?-i identity.json ?-s peer_key -m method -d data (| -f data_file) ?-t timeout_ms ?--dp dht_port ?--bn dht_bootstrap_nodes ?--ds dht_keypair_seed'
+
+if (argv.help) {
+  console.log(helpMsg)
+  process.exit(-1)
+}
+
+if (!argv.s) {
+  console.error('Error: peer invalid')
+  process.exit(-1)
+}
+
+if (argv.s) {
+  argv.s = libUtils.resolveHostToKey([], argv.s)
+}
+
+if (!argv.m) {
+  console.error('Error: method invalid')
+  process.exit(-1)
+}
+
+if (!argv.d && !argv.f) {
+  console.error('Error: data invalid')
+  process.exit(-1)
+}
+
+if (argv.f) {
+  const fpath = argv.f
+  argv.d = fs.readFileSync(fpath)
+}
+
+let keyPair = null
+
+if (argv.i) {
+  keyPair = libUtils.resolveIdentity([], argv.i)
+
+  if (!keyPair) {
+    console.error('Error: identity file invalid')
+    process.exit(-1)
+  }
+
+  keyPair = libKeys.parseKeyPair(keyPair)
+}
+
+const dhtOpts = {}
+
+if (argv.bn) {
+  dhtOpts.bootstrap = argv.bn.split(',').map(n => n.trim())
+}
+
+if (argv.dp) {
+  dhtOpts.port = +argv.dp
+}
+
+if (argv.ds) {
+  dhtOpts.keyPair = DHT.keyPair(Buffer.from(argv.ds, 'hex'))
+}
+
+if (!argv.t) {
+  argv.t = 5000
+}
+
 const main = async () => {
   try {
-    const argv = require('minimist')(process.argv.slice(2))
-
-    const helpMsg = 'Usage:\nhp-rpc-cli ?-i identity.json ?-s peer_key -m method -d data (| -f data_file) ?-t timeout_ms ?--dp dht_port ?--bn dht_bootstrap_nodes ?--ds dht_keypair_seed'
-
-    if (argv.help) {
-      console.log(helpMsg)
-      process.exit(-1)
-    }
-
-    if (!argv.s) {
-      console.error('Error: peer invalid')
-      process.exit(-1)
-    }
-
-    if (argv.s) {
-      argv.s = libUtils.resolveHostToKey([], argv.s)
-    }
-
-    if (!argv.m) {
-      console.error('Error: method invalid')
-      process.exit(-1)
-    }
-
-    if (!argv.d && !argv.f) {
-      console.error('Error: data invalid')
-      process.exit(-1)
-    }
-
-    if (argv.f) {
-      const fpath = argv.f
-      argv.d = fs.readFileSync(fpath)
-    }
-
-    let keyPair = null
-
-    if (argv.i) {
-      keyPair = libUtils.resolveIdentity([], argv.i)
-
-      if (!keyPair) {
-        console.error('Error: identity file invalid')
-        process.exit(-1)
-      }
-
-      keyPair = libKeys.parseKeyPair(keyPair)
-    }
-
-    const dhtOpts = {}
-    if (argv.bn) {
-      dhtOpts.bootstrap = argv.bn.split(',').map(n => n.trim())
-    }
-    if (argv.dp) {
-      dhtOpts.port = +argv.dp
-    }
-    if (argv.ds) {
-      dhtOpts.keyPair = DHT.keyPair(Buffer.from(argv.ds, 'hex'))
-    }
-
     let dht
     if (Object.keys(dhtOpts).length > 0) {
       dht = new DHT(dhtOpts)
@@ -77,10 +84,6 @@ const main = async () => {
       dht,
       keyPair
     })
-
-    if (!argv.t) {
-      argv.t = 5000
-    }
 
     const client = rpc.connect(Buffer.from(argv.s, 'hex'))
 
